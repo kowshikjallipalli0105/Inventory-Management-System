e34cd77e4902ba208eb3b7810d9d93fb96cb8cea
omteja04 <22P31A0539@acet.ac.in>
2024-04-04 22:12:17 +0530

    Update Quantity DB

---
+++ src/UpdateQuantityDB.java
@@ -0,0 +1,40 @@
+
+/**
+ * Author: omteja04
+ * Description: UpdateQuantityDB
+ */
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+public class UpdateQuantityDB {
+    public static void updateQuantityDB(Connection connection, String productId, int quantityToAdd) {
+        try {
+            PreparedStatement selectStatement = connection
+                    .prepareStatement("SELECT quantity FROM product WHERE product_id = ?");
+            selectStatement.setString(1, productId);
+            ResultSet resultSet = selectStatement.executeQuery();
+            int currentQuantity = 0;
+            while (resultSet.next()) {
+                resultSet.getInt("quantity");
+            }
+            resultSet.close();
+            selectStatement.close();
+            int newQuantity = currentQuantity + quantityToAdd;
+            PreparedStatement updateStatement = connection
+                    .prepareStatement("UPDATE product SET quantity = ? WHERE product_id = ?");
+            updateStatement.setInt(1, newQuantity);
+            updateStatement.setString(2, productId);
+            updateStatement.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Updated Successfully", "Success", JOptionPane.INFORMATION_MESSAGE);
+            updateStatement.close();
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
