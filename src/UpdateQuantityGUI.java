1f3c3613f07221439b100899e026cf4732793d9c
omteja04 <22P31A0539@acet.ac.in>
2024-04-05 11:29:28 +0530

    Update Quantity GUI

--- src/UpdateQuantityGUI.java
+++ src/UpdateQuantityGUI.java
@@ -3,9 +3,7 @@
  * Author: omteja04
  * Description: UpdateQuantityGUI
  */
-
 import java.sql.Connection;
-
 import javax.swing.JOptionPane;
 
 public class UpdateQuantityGUI {
@@ -21,9 +19,23 @@ public class UpdateQuantityGUI {
     public static void updateDialogue() {
 
         Connection connection = DatabaseConnection.getConnection();
-        productID = JOptionPane.showInputDialog("Enter The Product ID : ");
-        quantityToBeAdded = JOptionPane.showInputDialog("Enter The New Quantity : ");
-        quantity = Integer.parseInt(quantityToBeAdded);
+        if (connection != null) {
+            productID = JOptionPane.showInputDialog("Enter The Product ID : ");
+            quantityToBeAdded = JOptionPane.showInputDialog("Enter The New Quantity : ");
+
+            // Handle invalid input
+            try {
+                quantity = Integer.parseInt(quantityToBeAdded);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Quantity should be a numeric value.");
+                return; // Exit the method if the quantity is not numeric
+            }
+        }
         UpdateQuantityDB.updateQuantityDB(connection, productID, quantity);
+        // if (UpdateQuantityDB.updateQuantityDB(connection, productID, quantity)) {
+        // JOptionPane.showMessageDialog(null, "SuccessFully Updated");
+        // } else {
+        // JOptionPane.showMessageDialog(null, "Updating Failed");
+        // }
     }
 }
